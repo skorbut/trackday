0de14eccb577fec889e7aca45af0d814db6aaffb
Karsten Senz <karsten.senz@sumcumo.com>
2019-03-07 15:17:19 +0100

    add postgres configuration, fix datamodel

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'my precioussss'
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_DATABASE_URI = "postgresql://trackday:trackday@localhost:5432/trackday"
   SQLALCHEMY_TRACK_MODIFICATIONS = False
